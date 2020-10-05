14eefc5dbbd98a63c7a700f2a1e332b5743e40b4
Stas <bloodofabyss@yandex.ru>
2020-10-05 22:23:15 +0300

    Изменил конфиг для плагина babel

--- babel.config.js
+++ babel.config.js
@@ -3,16 +3,19 @@ const presets = [
     "@babel/env",
     {
       targets: {
-        edge: "17",
+        android: '67',
+        ios: '12',
+        edge: "15",
         ie: "11",
         firefox: "50",
         chrome: "64",
-        safari: "11.1",
+        safari: "11.1"
       },
       useBuiltIns: "usage",
       corejs: "3.4.1"
     }
-  ],
+  ]
 ];
 
 module.exports = { presets };
+
