c3e6d21344c93c5b88ad464d0d8830c213f47bff
Stas <bloodofabyss@yandex.ru>
2020-10-07 20:20:13 +0300

    Добавил небольшой код для открытия и закрытия dropdown меню при ширине менее 580px

--- src/pages/articles/index.js
+++ src/pages/articles/index.js
@@ -1,1 +1,14 @@
 import './index.css';
+
+// Мини код для попапа для ревью верстки
+const headerDropdownButton = document.querySelector('.header__dropdown-btn');
+const headerDropdownContainer = document.querySelector('.header__dropdown-container');
+const headerList = document.querySelector('.header__list');
+
+
+headerDropdownButton.addEventListener('click', () => {
+  headerList.classList.toggle('header__list_opened');
+  headerDropdownContainer.classList.toggle('popup_is-opened');
+  headerDropdownButton.classList.toggle('header__dropdown-btn_dark_closed')
+  headerDropdownButton.classList.toggle('header__dropdown-btn_dark_opened')
+});
